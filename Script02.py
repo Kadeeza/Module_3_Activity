c18249d5beefad793c28fa46125757ce9cbdb477
Kadeeza <dzawunor7@gmail.com>
2024-07-09 22:00:39 -0400

    I made a change

---
+++ Script02.py
@@ -0,0 +1,2 @@
+Name = input("Please enteryour name")
+print (f'Ah, so your name is{Name}')
---
+++ script01.py
@@ -0,0 +1,22 @@
+# Create a variable called 'name' that holds a string
+Name = "Ada"
+# Create a variable called 'country' that holds a string
+Country = "Canada"
+# Create a variable called 'age' that holds an integer
+Age = 50
+# Create a variable called 'hourly_wage' that holds an integer
+hourly_wage = 25
+# Calculate the daily wage for the user
+dailywage = hourly_wage/8
+# Create a variable called 'satisfied' that holds a boolean
+satisfied =True
+# Print out "Hello <name>!"
+print ("Hello" , Name)
+# Print out what country the user entered
+print ("Hello your country is" , Country)
+# Print out the user's age
+print (Age)
+# With an f-string, print out the daily wage that was calculated
+print (f'Your daily wage is {dailywage}')
+# With an f-string, print out whether the users were satisfied
+print (f'It is {satisfied} that you are super happy')
